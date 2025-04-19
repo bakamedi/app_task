b5cad0a8f25d0c541da905e9de7647d60100362a
Bakke Medina <bakamedi12@gmail.com>
2025-04-19 13:45:33 -0500

    UPD boton de añadir tarea

--- lib/app/presentation/modules/task/views/widgets/title_header_w.dart
+++ lib/app/presentation/modules/task/views/widgets/title_header_w.dart
@@ -9,9 +9,28 @@ class TitleHeaderW extends StatelessWidget {
 
     return Padding(
       padding: EdgeInsets.only(top: topPadding + 10, left: 16, right: 16),
-      child: Text(
-        'My Task',
-        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'My Task',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          InkWell(
+            onTap: () {
+              // acción del botón
+            },
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.grey.withValues(alpha: 0.2), // fondo con opacidad
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
       ),
     );
   }
