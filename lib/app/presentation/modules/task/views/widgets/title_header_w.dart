261064c9e862dd84622362a00288ef9ad372eb93
Bakke Medina <bakamedi12@gmail.com>
2025-04-21 08:04:35 -0500

    UPD header

--- lib/app/presentation/modules/task/views/widgets/title_header_w.dart
+++ lib/app/presentation/modules/task/views/widgets/title_header_w.dart
@@ -6,6 +6,7 @@ class TitleHeaderW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final topPadding = MediaQuery.of(context).padding.top;
+    final primaryColor = Theme.of(context).primaryColor;
 
     return Padding(
       padding: EdgeInsets.only(top: topPadding + 10, left: 16, right: 16),
@@ -13,7 +14,7 @@ class TitleHeaderW extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            'My Task',
+            'My Tasks',
             style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
           InkWell(
@@ -24,10 +25,10 @@ class TitleHeaderW extends StatelessWidget {
             child: Container(
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
-                color: Colors.grey.withValues(alpha: 0.2), // fondo con opacidad
+                color: primaryColor.withValues(alpha: 0.2),
                 shape: BoxShape.circle,
               ),
-              child: const Icon(Icons.add),
+              child: Icon(Icons.add, color: primaryColor),
             ),
           ),
         ],
