f12f013839d6c68ae9991f1d6964e5f248263e45
Bakke Medina <bakamedi12@gmail.com>
2025-05-04 13:59:32 -0500

    NEW se añade los logger

--- lib/app/core/injects_providers/store/store_inject_provider.dart
+++ lib/app/core/injects_providers/store/store_inject_provider.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_meedu/providers.dart';
+import 'package:logger/logger.dart';
 
 import '../../../data/source/providers/store/store_provider.dart';
 import '../db/db_inject_provider.dart';
+
+final _globalLogger = Logger(
+  level: bool.fromEnvironment('dart.vm.product') ? Level.warning : Level.debug,
+  printer: PrettyPrinter(
+    methodCount: 1,
+    errorMethodCount: 3,
+    lineLength: 80,
+    colors: true,
+    printEmojis: true,
+  ),
+);
 
 class StoreInjectProvider {
   StoreInjectProvider._();
@@ -10,6 +22,7 @@ class StoreInjectProvider {
     (ref) => StoreProvider(
       dbProvider: DbInjectProvider.dbInjectProvider.read(),
       storeName: 'my_store',
+      logger: _globalLogger,
     ),
   );
 }
